c33e7ca56ee1d6d5cb6b405d493c7ec21e3dba2a
Sam Morris <mail@sammorrisdesign.co.uk>
2016-03-02 17:11:15 +0000

    setup grunt

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+/*global module:false*/
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    watch: {
+      css: {
+        files: '_sass/**/*.scss',
+        tasks: ['sass']
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+            'assets/css/style.css' : '_sass/style.scss'
+        }
+      }
+    },
+    browserSync: {
+      bsFiles: {
+        src : ['assets/css/style.css', 'index.html', 'assets/js/*.js']
+      },
+      options: {
+        watchTask: true,
+        server: {
+          baseDir: "./"
+        }
+      }
+    }
+  });
+
+  // Tasks
+  grunt.registerTask('default', ['sass', 'browserSync', 'watch']);
+};
