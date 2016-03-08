6c539cd5dba78cd9692df80c298f4c079856c6ca
Sam Morris <mail@sammorrisdesign.co.uk>
2016-03-08 11:48:22 +0000

    Organise the javascriptz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,82 +1,28 @@
 $(document).ready(function() {
-    $(".button").click(function() {
-        var target = $(this).parent().next();
-        scrollToAnchor(target);
-
-        if ($(this).siblings("video").length) {
-            $(this).siblings("video").get(0).pause();
-        }
-    });
-
-    if (isMobile.any()) {
-        $("body").addClass("is-mobile");
-    } else {
-        $("body").addClass("is-desktop");
-    }
+    checkDeviceType();
 
     $(window).on('scroll', function() {
-        $('.panel').each(function() {
-            if($(this).visible(true)) {
-                if ($(this).offset().top - $(this).outerHeight() / 2 < $(window).scrollTop()) {
-                    $(".is-active").removeClass("is-active");
-                    $(this).addClass("is-active");
-                 }
-            } else {
-                $(this).removeClass("is-active");
-            }
-        });
+        setActivePanel();
+        autoPlayVideoOnDesktop();
+        toggleTag();
+    });
 
-        if (isMobile.any() == false) {
-            $(".panel--video").each(function() {
-                autoPlayVideoOnDesktop(this);
-            });
-        }
-
-        if($(".panel--header").hasClass("is-active") || $(".panel--last").hasClass("is-active")) {
-            $(".sponsor-tag").removeClass("is-shorter");
-        } else {
-            $(".sponsor-tag").addClass("is-shorter");
-        }
+    $(".button").click(function() {
+        scrollToPanel($(this).parent().next());
     });
 
     $(".button-sub--play").click(function() {
-        $(this).siblings("video").get(0).play();
+        playVideo($(this).siblings(".video").get(0));
     });
 
-    $(".button-sub--sound").click(function() {
-        var video = $(this).siblings("video");
-
-        if (video.get(0).muted) {
-            video.prop('muted', false);
-            $(this).addClass("is-muted");
-        } else {
-            video.prop('muted', true);
-            $(this).removeClass("is-muted");
-        }
+    $(".button-sub--sound").click(function(e) {
+        toggleSound($(this));
     });
 
-    $(".panel__video").click(function() {
-        console.log(this)
+    $(".video").click(function() {
+        togglePlayState($(this)[0]);
     });
 });
-
-function scrollToAnchor(e) {
-    var scrollTo = Math.ceil($(e).offset().top);
-
-    $("html, body").animate({
-        scrollTop: scrollTo
-    }, 'slow');
-}
-
-function autoPlayVideoOnDesktop(e) {
-    var video = $(e).find("video")[0];
-
-    if($(e).hasClass("is-active")) {
-        video.play();
-    } else if (!$(e).hasClass("is-active")) {
-        video.pause();
-    }
-}
 
 var isMobile = {
     Windows: function() {
@@ -95,3 +41,86 @@ var isMobile = {
         return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
     }
 };
+
+function checkDeviceType() {
+    if (isMobile.any()) {
+        $("body").addClass("is-mobile");
+    } else {
+        $("body").addClass("is-desktop");
+    }
+}
+
+function scrollToPanel(e) {
+    var scrollTo = Math.ceil($(e).offset().top);
+
+    $("html, body").animate({
+        scrollTop: scrollTo
+    }, 'slow');
+
+    if ($(this).siblings(".video").length) {
+        pauseVideo($(this).siblings(".video").get(0));
+    };
+}
+
+function toggleTag() {
+    if($(".panel--header").hasClass("is-active") || $(".panel--last").hasClass("is-active")) {
+        $(".sponsor-tag").removeClass("is-shorter");
+    } else {
+        $(".sponsor-tag").addClass("is-shorter");
+    }
+}
+
+function setActivePanel() {
+    $('.panel').each(function() {
+        if($(this).visible(true)) {
+            if ($(this).offset().top - $(this).outerHeight() / 2 < $(window).scrollTop()) {
+                $(".is-active").removeClass("is-active");
+                $(this).addClass("is-active");
+             }
+        } else {
+            $(this).removeClass("is-active");
+        }
+    });
+}
+
+// Video Functions
+function autoPlayVideoOnDesktop() {
+    if (isMobile.any() == false) {
+        $(".panel--video").each(function() {
+            var video = $(this).find(".video")[0];
+
+            if($(this).hasClass("is-active")) {
+                playVideo(video);
+            } else if (!$(this).hasClass("is-active")) {
+                pauseVideo(video);
+            }
+        });
+    }
+}
+
+function toggleSound(e) {
+    var video = e.siblings(".video");
+    if (video.get(0).muted) {
+        video.prop('muted', false);
+        e.addClass("is-muted");
+    } else {
+        video.prop('muted', true);
+        e.removeClass("is-muted");
+    }
+}
+
+function togglePlayState(e) {
+    if (e.paused) {
+        playVideo(e);
+    } else {
+        pauseVideo(e);
+    }
+}
+
+function pauseVideo(e) {
+    e.pause();
+}
+
+function playVideo(e) {
+    e.play();
+}
